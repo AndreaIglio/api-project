ee6a55ce2cd05127c34172e1983fc169e2064efe
Andrea Iglio <andrea.iglio@innoteam.it>
2024-03-02 00:20:07 +0000

    build: implemented show resources edit Voter

--- src/MultimediaResource/Repository/MultimediaResourceRepositoryInterface.php
+++ src/MultimediaResource/Repository/MultimediaResourceRepositoryInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\MultimediaResource\Repository;
 
 use App\MultimediaResource\Entity\MultimediaResource;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Uid\Uuid;
 
 interface MultimediaResourceRepositoryInterface
@@ -16,6 +17,21 @@ interface MultimediaResourceRepositoryInterface
     public function findOneById(Uuid $id): ?MultimediaResource;
 
     public function remove(MultimediaResource $multimediaResource): void;
+
+    /**
+     * @return Collection<array-key, MultimediaResource>
+     */
+    public function findAllMultimediaResources(): Collection;
+
+    /**
+     * @return Collection<array-key, MultimediaResource>
+     */
+    public function findByCustomerId(Uuid $customerId): Collection;
+
+    /**
+     * @return Collection<array-key, MultimediaResource>
+     */
+    public function findByManagerId(Uuid $managerId): Collection;
 
     public function flush(): void;
 }
